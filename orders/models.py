43c7497cb9311a9630bede6ce8261a4523b7e12f
edumats <eduardo.mats@gmail.com>
2020-01-17 11:42:48 -0300

    Updated Django required version

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Food(models.Model):
-    quantity = models.IntergerField()
+    quantity = models.IntegerField()
 
 class Topping(models.Model):
     name = models.CharField(max_length=64)
@@ -10,5 +10,7 @@ class Topping(models.Model):
 
 class Pizza(models.Model):
     toppings = models.ManyToManyField(Topping)
+    price = models.DecimalField(max_digits=100, decimal_places=2)
 
-class Order(models.Model):
+class Cart(models.Model):
+    pass
