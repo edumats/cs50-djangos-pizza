a70197da2ee396818546dfffc61656c452872d9e
Eduardo Matsuoka <36085476+edumats@users.noreply.github.com>
2020-01-22 12:11:11 -0300

    Update models.py
    Added Comment

--- orders/models.py
+++ orders/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
+# General Model
 class Food(models.Model):
     name = models.CharField(max_length=70)
     price = models.DecimalField(max_digits=5, decimal_places=2)
