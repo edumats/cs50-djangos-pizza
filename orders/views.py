93bb3a8ceaf138b684d9ee34486e2f506062d9e9
edumats <eduardo.mats@gmail.com>
2020-01-23 16:15:07 -0300

    Added page templates

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    context = {
+        "pastas": Pasta.objects.all()
+    }
+    return render(request, "orders/index.html", context)
