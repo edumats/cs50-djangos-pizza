17f3aaf643a507c4d26cc3c52d0d5009a1ce0391
edumats <eduardo.mats@gmail.com>
2020-01-23 12:13:21 -0300

    Working models, added most of products, except pizza and sub

--- orders/admin.py
+++ orders/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from .models import Salad, Dinner, Pasta, Pizza, Sub, Topping
-# Register your models here.
+from .models import *
 
 admin.site.register(Salad)
 admin.site.register(Dinner)
 admin.site.register(Pasta)
 admin.site.register(Pizza)
+admin.site.register(PizzaTopping)
+admin.site.register(PizzaType)
 admin.site.register(Sub)
-admin.site.register(Topping)
+admin.site.register(SubTopping)
+admin.site.register(SubType)
--- orders/models.py
+++ orders/models.py
@@ -1,31 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-
-# General Model
-class Food(models.Model):
-    name = models.CharField(max_length=70)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.name} {self.price}"
-
 class Dinner(models.Model):
     DINNER_SIZES = [
         ('S', 'Small'),
         ('L', 'Large')
     ]
-    name = models.ForeignKey('Food', on_delete=models.CASCADE, related_name="dinners")
+    name = models.CharField(max_length=70)
     size = models.CharField(max_length=1, choices=DINNER_SIZES)
+    price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
+    def __str__(self):
+        return f"{self.name} - Size: {self.size} - $ {self.price}"
 
 class Salad(models.Model):
-    name = models.ForeignKey('Food', on_delete=models.CASCADE)
+    name = models.CharField(max_length=70)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return f"{self.name} - $ {self.price}"
+
 class Pasta(models.Model):
-    name = models.ForeignKey('Food', on_delete=models.CASCADE)
+    name = models.CharField(max_length=70)
     price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} - $ {self.price}"
 
 class Pizza(models.Model):
     PIZZA_CHOICES = [
@@ -37,15 +37,24 @@ class Pizza(models.Model):
         ('L', 'Large')
     ]
     size = models.CharField(max_length=1, choices=PIZZA_SIZES)
-    type = models.ManyToManyField('Pizza_Type')
+    type = models.ManyToManyField('PizzaType')
     price = models.DecimalField(max_digits=5, decimal_places=2)
-    toppings = models.ManyToManyField('Pizza_Topping')
+    toppings = models.ManyToManyField('PizzaTopping')
 
-class Pizza_Topping(models.Model):
+    def __str__(self):
+        return f"{self.type} - Size: {self.size} - Toppings: {self.toppings} : $ {self.price}"
+
+class PizzaTopping(models.Model):
     name = models.CharField(max_length=70)
 
-class Pizza_Type(models.Model):
+    def __str__(self):
+        return f"{self.name}"
+
+class PizzaType(models.Model):
     name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f"{self.name}"
 
 class Sub(models.Model):
     SUB_SIZES = [
@@ -53,14 +62,22 @@ class Sub(models.Model):
         ('L', 'Large')
     ]
     size = models.CharField(max_length=1, choices=SUB_SIZES)
-    name = models.ForeignKey('Food', on_delete=models.CASCADE)
-    type = models.ForeignKey('Sub_Type', on_delete=models.CASCADE)
-    topping = models.ManyToManyField('Sub_Topping')
+    type = models.ForeignKey('SubType', on_delete=models.CASCADE)
+    topping = models.ManyToManyField('SubTopping')
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
-class Sub_Topping(models.Model):
+    def __str__(self):
+        return f"{self.type} - Size: {self.size} - Toppings: {self.toppings} : $ {self.price}"
+
+class SubTopping(models.Model):
     name = models.CharField(max_length=70)
     price = models.DecimalField(max_digits=5, decimal_places=2)
 
-class Sub_Type(models.Model):
+    def __str__(self):
+        return f"{self.name} : $ {self.price}"
+
+class SubType(models.Model):
     name = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f"{self.name}"
