d643085618240ef244c9e8481795a0c1ba55fe4a
edumats <eduardo.mats@gmail.com>
2020-01-24 15:57:29 -0300

    Added users app, but needs to redirect to correct template

--- orders/urls.py
+++ orders/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 
 from . import views
 
+from users import views as user_views
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login", views.login_view, name="login"),
+    path("logout", views.logout_view, name="logout"),
+    path("register", user_views.register_view, name="register")
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,11 +1,30 @@
-from django.http import HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from .models import *
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        return render(request, "users/login.html", {"message": None})
     context = {
+        "user": request.user,
         "pastas": Pasta.objects.all()
     }
     return render(request, "orders/index.html", context)
+
+def login_view(request):
+    username = request.POST["username"]
+    password = request.POST["password"]
+    user = authenticate(request, username=username, password=password)
+    if User is not None:
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "users/login.html", {"message": "Invalid credentials."})
+
+def logout_view(request):
+    logout(request)
+    return render(request, "users/login.html", {"message": "Logged out."})
